9100960e50bda16dd8ebacba0482ffcdb4a2f2ed
krisee <krishiyana19@gmail.com>
2022-03-10 21:54:57 +0545

    express framework used

--- NodeJs/express_intro/server.js
+++ NodeJs/express_intro/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 // import { Router } from 'express'; 
 
-const server = express();
 
+const server = express();
 const loggingMiddleware = (req, res, next) => {
   const url = req.url;
   const method = req.method;
@@ -12,28 +12,24 @@ const loggingMiddleware = (req, res, next) => {
 
   next();
 };
-
-server.use(bodyParser.json());
 server.use(loggingMiddleware);
+server.use(bodyParser.json());
 
-server.get('/', (req, res, next) => {
+server.get('/', (req, res, next)=>{
   res.send('This is the response from the index(/) route');
 });
-
 server.get('/cars', (req, res, next) => {
   res.send('This is the response from the cars(/cars) route');
 });
-
 server.post('/cars', (req, res, next) => {
   console.log(req.body);
 
   res.send('This is the response from post request for the car');
 });
-
 const PORT = 8848;
 
 server.listen(PORT, () => {
-  console.log(`Listening on 127.0.0.1:${PORT}`);
+  console.log(`Listening on 127.0.0.1:${PORT}\n`);
 });
 
 // GET /cars -> Get all cars
