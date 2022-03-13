7f50261bee961aca3368f6d96ef71e0211d93e69
krisee <krishiyana19@gmail.com>
2022-03-13 22:40:16 +0545

    state management concept, redux

--- React/classwork/src/components/App.js
+++ React/classwork/src/components/App.js
@@ -5,6 +5,39 @@ import SpeedIndicator from "./SpeedIndicator";
 import Click from "./Click";
 import Hover from "./Hover";
 import Calculator from "./Calculator";
+import Heading from "./Heading";
+
+// expression ? value1 : value2
+// HP.getPrice(){    }
+//
+
+// function Book(name, page) {
+//   this.name = name;
+//   this.page = page;
+
+//   this.calculatePrice = function (rate) {
+//     return this.page * rate;
+//   };
+// }
+
+// const HP = new Book("HP", 200);
+
+// // Implicit Binding
+// console.log(HP.calculatePrice(20));
+
+// const peaterPan = {
+//   page: 90,
+// };
+
+// // Explicit Binding
+// console.log(HP.calculatePrice.call(peaterPan, 20));
+// console.log(HP.calculatePrice.apply(peaterPan, [20]));
+
+// const calculatePeaterPansPrice = HP.calculatePrice.bind(peaterPan);
+
+// console.log(calculatePeaterPansPrice(20));
+
+export const listContext = React.createContext("list");
 
 class App extends React.Component {
   constructor() {
@@ -32,17 +65,31 @@ class App extends React.Component {
       <React.Fragment>
         <Welcome name="World" />
         <SpeedIndicator />
-        <h1>Number of fruits = {this.state.list.length} </h1>
 
-        <List list={this.state.list} onAdd={this.handleListAdd} />
+        <listContext.Provider value={[this.state.list, this.handleListAdd]}>
+          <Heading />
+          <List />
+        </listContext.Provider>
+
         <Click />
         <Hover />
 
-        <Calculator />
+        <Calculator>
+          <h1>Hello </h1>
+        </Calculator>
       </React.Fragment>
     );
   }
 }
 
+// handleListAdd();
+
+//    APP
+//     |  \
+//    List Heading
+//   /   \
+// Items
+//
+// Ul   Li
 
 export default App;
